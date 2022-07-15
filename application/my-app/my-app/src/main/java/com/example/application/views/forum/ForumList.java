bcd6802b3237f5d0b1880f36d2e946290df0677d
ktleong2000 <brendanleong105@gmail.com>
2022-07-14 17:57:56 -0700

    Forum Post test 3

--- application/my-app/my-app/src/main/java/com/example/application/views/forum/ForumList.java
+++ application/my-app/my-app/src/main/java/com/example/application/views/forum/ForumList.java
@@ -3,6 +3,7 @@ package com.example.application.views.forum;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.ClickNotifier;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -16,15 +17,19 @@ import com.vaadin.flow.router.Route;
 @Route(value = "forumList", layout = MainLayout.class)
 public class ForumList extends VerticalLayout {
     Button search = new Button("Search");
+    Button newPost = new Button("Make a new post", e ->{
+        this.getUI().ifPresent(ui -> ui.navigate("/forumPost"));
+    });
+
     public ForumList(){
         TextField searchBar = new TextField();
-        searchBar.setPlaceholder("Search for a forum post");
+        searchBar.setPlaceholder("Search for a forum post (Title, Description, User)");
         searchBar.setPrefixComponent(VaadinIcon.SEARCH.create());
         searchBar.setClearButtonVisible(true);
         searchBar.setWidth("50em");
 
 
-        HorizontalLayout hv = new HorizontalLayout(searchBar, search);
+        HorizontalLayout hv = new HorizontalLayout(searchBar, search, newPost);
         setHorizontalComponentAlignment(Alignment.CENTER, hv);
         hv.setPadding(true);
 
---
+++ application/my-app/my-app/src/main/java/com/example/application/views/forum/ForumPost.java
@@ -0,0 +1,18 @@
+package com.example.application.views.forum;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.ClickNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Forum Post")
+@Route(value = "forumPost", layout = MainLayout.class)
+public class ForumPost extends VerticalLayout{
+    
+}
