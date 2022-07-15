70b1fc03f7b5bc53fea4d4241405f7391b69c22e
Jwong66 <jwong66@mail.sfsu.edu>
2022-07-14 17:26:15 -0700

    ShoppingCartView

--- application/my-app/my-app/src/main/java/com/example/application/views/ShoppingCart/ShoppingCartView.java
+++ application/my-app/my-app/src/main/java/com/example/application/views/ShoppingCart/ShoppingCartView.java
@@ -2,9 +2,14 @@ package com.example.application.views.ShoppingCart;
 
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -58,11 +63,39 @@ public class ShoppingCartView extends VerticalLayout{
         for (HashMap<String, String> i: cart){
             HorizontalLayout horizontal = new HorizontalLayout();
            
-            // System.out.println(i.get("title"));
-            horizontal.add(i.get("title"));
-            horizontal.add(i.get("image"));
-            horizontal.add(i.get("price"));
-            horizontal.add(i.get("quantity"));
+
+            // // System.out.println(i.get("title"));
+            // horizontal.add(i.get("title"));
+            // horizontal.add(i.get("image"));
+            // horizontal.add(i.get("price"));
+            // horizontal.add(i.get("quantity"));
+
+            // add(horizontal);
+
+            horizontal.setWidthFull();
+
+            String url = i.get("image");
+            
+            TextField title = new TextField("Name");
+            Image image = new Image(url,"");
+            image.setHeight("200px");
+            image.setWidth("150px");
+            // TextField image = new TextField();
+            TextField quantity = new TextField("Quantity");
+            TextField price = new TextField("Price");
+
+            Button addToCart = new Button("Remove from Cart");
+            addToCart.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+            title.setValue(i.get("title"));
+            title.setReadOnly(true);
+            image.setTitle(i.get("image"));
+            quantity.setValue(i.get("quantity"));
+            price.setValue(i.get("price"));
+
+
+            horizontal.add(title, image, quantity, price, addToCart);
+            horizontal.setJustifyContentMode(JustifyContentMode.START);
 
             add(horizontal);
         } 
