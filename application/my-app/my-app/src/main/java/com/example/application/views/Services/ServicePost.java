a0d11ad1466a1c206143977586e8d1096e90a814
ktleong2000 <brendanleong105@gmail.com>
2022-07-18 20:42:35 -0700

    Service Listing CSS updated

--- application/my-app/my-app/src/main/java/com/example/application/views/Services/ServicePost.java
+++ application/my-app/my-app/src/main/java/com/example/application/views/Services/ServicePost.java
@@ -3,7 +3,9 @@ package com.example.application.views.Services;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.ClickNotifier;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
@@ -16,34 +18,56 @@ import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+@CssImport("./themes/myapp/makeservicepost.css")
 @PageTitle("Service Post")
 @Route(value = "servicePost", layout = MainLayout.class)
 public class ServicePost extends VerticalLayout{
 
     public ServicePost(){
-        //The title of the make a forum post page
-        add(new H1("Make a new Post!"));
+        getElement().getClassList().add("make-forum-grid");
+        setWidth("100%");
 
+        //The title of the make a forum post page
+        this.addClassName("forum-background");
+
+        Div headerholder = new Div();
+        headerholder.addClassName("header");
+        H1 header = new H1("Make a new Post!");
+        header.addClassName("header");
+
+
+        Div titleholder = new Div();
+        titleholder.addClassName("make-title");
         TextField title= new TextField("Title");
-        TextField school= new TextField("School");
+        title.addClassName("make-title");
+
+
+        Div descriptionholder = new Div();
+        descriptionholder.addClassName("make-description");
         TextArea description = new TextArea("Description");
+        description.addClassName("make-description");
         description.setWidth("800px");
         description.setHeight("200px");
 
+        Div submitholder = new Div();
+        submitholder.addClassName("make-submit-button");
+        Button submit = new Button("Submit", event->{
+            //var fPost = new fPost();
+            //binder.writeBeanIfValid(fPost);
+            Notification.show("Service Listing was saved");
+        });
+        submit.addClassName("make-submit-button");
+
         //This should create a new post object which will later be stored on the database to be accessed later
         //Binder will make it so all inputs into the fields will alter the object properties
-        // var binder = new Binder<>(sPost.class);
+        // var binder = new Binder<>(fPost.class);
         // binder.bindInstanceFields(this);
 
         add(
+            header,
             title,
-            school,
             description,
-            new Button("Submit", event->{
-                //var fPost = new fPost();
-                //binder.writeBeanIfValid(fPost);
-                Notification.show("Service Listing was saved");
-            })
+            submit
         );
     }
 }
--- application/my-app/my-app/src/main/java/com/example/application/views/forum/ForumPost.java
+++ application/my-app/my-app/src/main/java/com/example/application/views/forum/ForumPost.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.ClickNotifier;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
@@ -23,19 +24,39 @@ import com.vaadin.flow.router.Route;
 public class ForumPost extends VerticalLayout{
 
     public ForumPost(){
+        getElement().getClassList().add("make-forum-grid");
+        setWidth("100%");
+
         //The title of the make a forum post page
         this.addClassName("forum-background");
 
+        Div headerholder = new Div();
+        headerholder.addClassName("header");
         H1 header = new H1("Make a new Post!");
         header.addClassName("header");
 
+
+        Div titleholder = new Div();
+        titleholder.addClassName("make-title");
         TextField title= new TextField("Title");
         title.addClassName("make-title");
 
+
+        Div descriptionholder = new Div();
+        descriptionholder.addClassName("make-description");
         TextArea description = new TextArea("Description");
         description.addClassName("make-description");
         description.setWidth("800px");
         description.setHeight("200px");
+
+        Div submitholder = new Div();
+        submitholder.addClassName("make-submit-button");
+        Button submit = new Button("Submit", event->{
+            //var fPost = new fPost();
+            //binder.writeBeanIfValid(fPost);
+            Notification.show("Service Listing was saved");
+        });
+        submit.addClassName("make-submit-button");
 
         //This should create a new post object which will later be stored on the database to be accessed later
         //Binder will make it so all inputs into the fields will alter the object properties
@@ -46,11 +67,7 @@ public class ForumPost extends VerticalLayout{
             header,
             title,
             description,
-            new Button("Submit", event->{
-                //var fPost = new fPost();
-                //binder.writeBeanIfValid(fPost);
-                Notification.show("Post was saved");
-            })
+            submit
         );
     }
 }
